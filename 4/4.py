6d7254449dc355162fdd19deb8b56385c15f46e6
MartinOravecSvK <oravec.martin01@gmail.com>
2022-12-04 05:25:01 +0000

    Problem 4 solved

--- 4/4.py
+++ 4/4.py
@@ -6,7 +6,38 @@ def main():
     data = file.read().split("\n")
     file.close()
 
-    
+    total = 0
+    for pair in data:
+        pair = pair.split(",")
+        mem1 = pair[0].split("-")
+        mem2 = pair[1].split("-")
+
+        if (int(mem1[0]) <= int(mem2[0]) and int(mem1[1]) >= int(mem2[1])):
+            total += 1
+            continue
+        if (int(mem1[0]) >= int(mem2[0]) and int(mem1[1]) <= int(mem2[1])):
+            total += 1
+            continue
+                
+    print("Part one")
+    print(total)
+
+    total = 0
+
+    for pair in data:
+        pair = pair.split(",")
+        mem1 = pair[0].split("-")
+        mem2 = pair[1].split("-")
+
+        if (int(mem1[0]) <= int(mem2[0]) and int(mem1[1]) >= int(mem2[0])):
+            total += 1
+            continue
+        if (int(mem1[0]) >= int(mem2[0]) and int(mem1[0]) <= int(mem2[1])):
+            total += 1
+            continue
+
+    print("Part two")
+    print(total)
 
 if __name__ == "__main__":
     main()
