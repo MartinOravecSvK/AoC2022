d4f809678afc35550e9e5c35a2d122cbbb52cf43
MartinOravecSvK <oravec.martin01@gmail.com>
2022-12-06 05:32:10 +0000

    Problem 7 init + future folder init

---
+++ 16/16.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+def main():
+    file = open(os.path.join(sys.path[0], "input.txt"), "r")
+    data = file.read()
+    file.close()
+
+    
+
+if __name__ == "__main__":
+    main()
