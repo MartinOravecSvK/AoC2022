8a453ff63c4496704884b45f4e2a2925cfebc590
MartinOravecSvK <oravec.martin01@gmail.com>
2022-12-06 02:41:15 +0000

    Problem 5 solved

--- 5/5.py
+++ 5/5.py
@@ -1,10 +1,73 @@
 import os
 import sys
+import re
 
 def main():
-    file = open(os.path.join(sys.path[0], "input"), "r")
+    file = open(os.path.join(sys.path[0], "input.txt"), "r")
     data = file.read().split("\n")
     file.close()
 
+    stacks = [[], [], [], [], [], [], [], [], []]
+    iI = data.index("")
+
+    for line in data[:iI-1]:
+        line = line.replace("[", "").replace("]", "").replace("    " , "%").replace("   " , "%").replace(" ", "")
+        counter = 0
+        for crate in line:
+            if (crate == "%"):
+                counter += 1
+                continue
+            stacks[counter].append(crate)
+            counter += 1
+
+    for stack in stacks:
+        stack.reverse()
+
+    for instruction in data[iI+1:]:
+        instruction = instruction.split(" ")
+        q = int(instruction[1])
+        f = int(instruction[3])
+        t = int(instruction[5])
+        for i in range(q):
+            stacks[t-1].append(stacks[f-1].pop())
+
+    print("Part One:")
+    for stack in stacks:
+        print(stack.pop(), end="")
+    print()
+
+    stacks = [[], [], [], [], [], [], [], [], []]
+    iI = data.index("")
+
+    for line in data[:iI-1]:
+        line = line.replace("[", "").replace("]", "").replace("    " , "%").replace("   " , "%").replace(" ", "")
+        counter = 0
+        for crate in line:
+            if (crate == "%"):
+                counter += 1
+                continue
+            stacks[counter].append(crate)
+            counter += 1
+
+    for stack in stacks:
+        stack.reverse()
+
+    for instruction in data[iI+1:]:
+        instruction = instruction.split(" ")
+        q = int(instruction[1])
+        f = int(instruction[3])
+        t = int(instruction[5])
+        tmp = []
+        for i in range(q):
+            tmp.append(stacks[f-1].pop())
+        tmp.reverse()
+        for crate in tmp:
+            stacks[t-1].append(crate)
+
+    print("Part Two:")
+    for stack in stacks:
+        print(stack.pop(), end="")
+    print()
+    
 if __name__ == "__main__":
     main()
