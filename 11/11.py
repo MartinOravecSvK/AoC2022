eb4bb48f471406de7b492c87ced7de6e4cf4e6e7
MartinOravecSvK <oravec.martin01@gmail.com>
2022-12-11 08:06:05 +0000

    Problem 11 solved

--- 11/11.py
+++ 11/11.py
@@ -25,7 +25,7 @@ def main():
         elif ("false:" in line):
             monkeys[cM][4] = line[9]
 
-    for round in range(20):
+    for _ in range(20):
         for monkey in monkeys.keys():
             for item in range(len(monkeys[monkey][0])):
                 monkeys[monkey][5] += 1
@@ -42,7 +42,7 @@ def main():
                 else:
                     w = int(int(opp[0]) / int(opp[2]))
                 
-                # w = int(w/3)
+                w = int(w/3)
 
                 if(w%int(monkeys[monkey][2]) == 0):
                     monkeys[monkeys[monkey][3]][0].append(w)
@@ -75,42 +75,57 @@ def main():
             monkeys[cM][4] = line[9]
 
     divs = [ int(monkeys[x][2]) for x in monkeys.keys() ]
-    check = lambda x, y: map(lambda div: x%div == y%div, divs)
+    monkeys2 = monkeys.copy()
 
-    for round in range(10000):
-        print(round)
+    for monkey in monkeys.keys():
+        newA = []
+        a = len(monkeys[monkey][0])
+        for i in range(a):
+            item = monkeys[monkey][0][i]
+            itemlist = [ item%div for div in divs ]
+            fullItem = {}
+            c = 0
+            for monkey2 in monkeys.keys():
+                fullItem[monkey2] = itemlist[c]
+                c += 1
+            newA.append(fullItem)
+        monkeys2[monkey][0] = newA
+
+    monkeys = monkeys2.copy()
+
+    for _ in range(10000):
         for monkey in monkeys.keys():
             for item in range(len(monkeys[monkey][0])):
                 monkeys[monkey][5] += 1
                 item = monkeys[monkey][0].pop()
-                w = 0
-                opp = monkeys[monkey][1]
-                opp = list(map(lambda x : x.replace("old", str(item)), opp))
-                if (opp[1] == '+'):
-                    w = int(opp[0]) + int(opp[2])
-                elif (opp[1] == '-'):
-                    w = int(opp[0]) - int(opp[2])
-                elif (opp[1] == '*'):
-                    w = int(opp[0]) * int(opp[2])
+                fopp = monkeys[monkey][1]
+                fullItem = {}
+                for i in item.keys():
+                    w = 0
+                    opp = list(map(lambda x : x.replace("old", str(item[i])), fopp))
+                    if (opp[1] == '+'):
+                        w = int(opp[0]) + int(opp[2])
+                    elif (opp[1] == '-'):
+                        w = int(opp[0]) - int(opp[2])
+                    elif (opp[1] == '*'):
+                        w = int(opp[0]) * int(opp[2])
+                    else:
+                        w = int(int(opp[0]) / int(opp[2]))
+                    fullItem[i] = w%int(monkeys[i][2])
+                if(fullItem[monkey] == 0):
+                    monkeys[monkeys[monkey][3]][0].append(fullItem)
                 else:
-                    w = int(int(opp[0]) / int(opp[2]))
-
-                if(w%int(monkeys[monkey][2]) == 0):
-                    i = 0
-                    while (False in check(w, i)):
-                        i+=1
-                    monkeys[monkeys[monkey][3]][0].append(w)
-                else:
-                    i = 0
-                    while (False in check(w, i)):
-                        i+=1
-                    monkeys[monkeys[monkey][4]][0].append(w)
+                    monkeys[monkeys[monkey][4]][0].append(fullItem)
     
     result = [ monkeys[x][5] for x in monkeys.keys()]
     result.sort()
     result.reverse()
     print("Part two:")
     print(result[0] * result[1])
+    
+    # 2713310158
+    # 2876069637
+    # 833266668
 
 if __name__ == "__main__":
     main()
