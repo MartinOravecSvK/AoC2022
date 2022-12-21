3c162256ced3fb00499e36ff6c366cd12037a356
MartinOravecSvK <oravec.martin01@gmail.com>
2022-12-22 00:24:09 +0100

    Problem 14 solved

--- 14/14.py
+++ 14/14.py
@@ -9,7 +9,9 @@ def main():
     # if not in area -> air
     area = {}
     maxX = 0
+    minX = 1000000
     maxY = 0
+    minY = 0
     points = []
     c = 0
     for lines in data:
@@ -21,6 +23,7 @@ def main():
             # might be useful
             maxX = max(maxX, int(line[0]))
             maxY = max(maxY, int(line[1]))
+            minX = min(minX, int(line[0]))
         c += 1
 
     for line in range(len(points)):
@@ -38,11 +41,12 @@ def main():
                 dy = 1
 
             for i in range(0, max(abs(a[0]-b[0]), abs(a[1]-b[1]))+1):
-                area[str(a[0]+dx*i)+":"+str(a[1]+dy*i)] = True
+                area[str(a[0]+dx*i)+":"+str(a[1]+dy*i)] = "#"
 
+    originalArea = area.copy()
     x, y = 500, 0
     s = 0
-    while (y <= maxY+10):
+    while (y <= maxY):
         if not area.get(str(x)+":"+str(y+1), False):
             y += 1
         elif not area.get(str(x-1)+":"+str(y+1), False):
@@ -57,6 +61,48 @@ def main():
             x, y = 500, 0
 
     print("Part one:")
+    print(s)
+
+    floor = maxY + 2
+    area = originalArea
+
+    for i in range(0, 2*maxX):
+        area[str(1*i)+":"+str(floor)] = "#"
+
+    x, y = 500, 0
+    s = 0
+    while True:
+        if not area.get(str(x)+":"+str(y+1), False) and y != floor:
+            y += 1
+        elif not area.get(str(x-1)+":"+str(y+1), False) and y != floor:
+            x -= 1
+            y += 1
+        elif not area.get(str(x+1)+":"+str(y+1), False) and y != floor:
+            x += 1
+            y += 1
+        elif (x == 500 and y == 0):
+            area[str(x)+":"+str(y)] = "O"
+            s += 1
+            break
+        else :
+            maxX = max(maxX, x)
+            minX = min(minX, x)
+            area[str(x)+":"+str(y)] = "O"
+            s += 1
+            x, y = 500, 0
+
+    # Visualizer :D (for part two, you can change it a bit to make it work for the first part)
+
+    # for line in range(floor+1):
+    #     print()
+    #     for point in range(minX-1, maxX+2):
+    #         if (area.get(str(point)+":"+str(line), False)):
+    #             print(area.get(str(point)+":"+str(line)), end="")
+    #         else :
+    #             print(".", end="")
+    # print()
+
+    print("Part two:")
     print(s)
 
 if __name__ == "__main__":
