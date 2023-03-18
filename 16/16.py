1feb94ffd6770dde17b6c284b16956f44fd9a2c9
MartinOravecSvK <oravec.martin01@gmail.com>
2023-03-18 01:39:50 +0000

    revamped the code for Problem 16

--- 16/16.py
+++ 16/16.py
@@ -1,32 +1,58 @@
 import os
 import sys
 
-def dij(queue, valves, dist, dists, distLimit):
+def queueF(queue):
+    newQueue = [queue.pop(0)]
 
-    valve = queue.pop()
-    dists.append((valve, dist))
-    tunnels = valves[valve][1]
-    dist += 1
+    for item in queue[1:]:
+        i = 0
+        while (item[1]>newQueue[i][1]):
+            i += 1
+            if (len(newQueue) <= i):
+                break
+        newQueue = newQueue[:i] + [item] + newQueue[i+1:]
 
-    for tunnel in [ x for x in tunnels if x not in [ x[0] for x in dists ] + queue ]:
-        queue.append(tunnel)
+    return newQueue
 
-    if (dist > distLimit or queue == []):
+def dij(queue, graph, maxDistance, reachedNodes):
+    currentNode = queue.pop(0)
+    currentDistance = currentNode[1]
+    if (currentNode in reachedNodes):
         return
+    reachedNodes.append(currentNode)
 
-    dij(queue, valves, dist, dists, distLimit)
+    adjacentNodes = graph[currentNode[0]][1]
+    for node in adjacentNodes:
+        if (node in [ x[0] for x in reachedNodes ]):
+            continue
+        queue.append((node, currentDistance+1))
 
-def traverse(valve, valves, timeLimit, pressure):
+    if (len(queue) == 0 or currentDistance >= maxDistance):
+        return
+    
+    queue = queueF(queue)
+    dij(queue, graph, maxDistance, reachedNodes)
+
+def maximumPressure(valve, valves, timeLimit, pressure):
+    nextValve = valve
+    openValves = []
 
     while (timeLimit >= 0):
+        best = ["", 0, 0]
+        valvePriorityList = []
+        dij([(nextValve, 0)], valves, timeLimit, valvePriorityList)
 
-        nextValve = valve
-        dists = []
-        dij([nextValve], valves, 0, dists, timeLimit)
-        pressure += valves[max(dists)[0]][0] * (timeLimit - (max(dists)[1] + 1))
-        timeLimit -= max(dists)[1]+1
+        for i in range(len(valvePriorityList)):
+            v, timeUsed = valvePriorityList[i]
+            if ((timeLimit-timeUsed)*valves[v][0] > best[1] and v not in openValves):
+                best = [v, (timeLimit-timeUsed)*valves[v][0], timeUsed]
         
-        nextValve = max(dists)[0]
+        nextValve = best[0]
+        if (nextValve == ""):
+            break
+        pressure += best[1]
+        timeLimit -= best[2]
+        openValves.append(nextValve)
 
     return pressure
 
@@ -49,9 +75,8 @@ def main():
             maxOpenValves.append(valve[1])
         valves[valve[1]] = (int(flow), tunnels)
 
-    print(traverse("AA", valves, time, 0))
+    maxPressure = maximumPressure("AA", valves, time, 0)
 
-    maxPressure = 0
     print("Part one:")
     print(maxPressure)
 
